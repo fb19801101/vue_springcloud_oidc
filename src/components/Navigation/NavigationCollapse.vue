39771ed94543b1387afdecd99f738875b8dd8899
Administrator <fb19801101@126.com>
2022-03-19 14:04:02 +0800

    flex style

---
+++ src/components/Navigation/NavigationCollapse.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="flex-collapse-container">
+    <el-scrollbar class="flex-collapse-scroll" :native = "false">
+      <el-collapse class="flex-collapse-body" @change="handleChange" accordion>
+        <el-collapse-item
+          v-for="(item, index) in items"
+          :key="index"
+          :name="item.name"
+          :disabled="item.disabled">
+          <template slot="title">
+            <span class="box-margin-left-10">{{item.title}}</span>
+            <i :class="item.icon + ' box-margin-left-10'"></i>
+          </template>
+          <div v-for="(text, key) in item.content" :key="key">{{text}}</div>
+        </el-collapse-item>
+      </el-collapse>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavigationCollapse',
+  props: {
+    // [
+    //   { name: 'PlanParams',
+    //     title: '网络计算参数',
+    //     icon: 'el-icon-info',
+    //     content: ['甘忠忠：股份公司/十二局集团', '薛朝阳：股份公司/十一局集团'],
+    //     disabled: false },
+    //   { name: 'ApiHolder',
+    //     title: 'API注册信息',
+    //     icon: 'el-icon-info',
+    //     content: ['甘忠忠：股份公司/十二局集团/部长/甘忠忠', '原岗位：股份公司/十二局集团/部员/甘忠忠'],
+    //     disabled: false },
+    //   { name: 'BrowserWeb',
+    //     title: '网页端页面',
+    //     icon: 'el-icon-info',
+    //     content: ['十二局一公司：股份公司/十二局集团/十二局集团一公司'],
+    //     disabled: false }
+    // ]
+    items: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleChange (name) {
+      this.$store.dispatch('updateTabPaneName', name)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .flex-collapse-container >>> {
+    flex-display();
+    flex-direction(column);
+    flex-justify-content(space-between);
+    height: calc(40vh - 170px);
+    .flex-collapse-scroll {
+      flex(1);
+      white-space: nowrap;
+      overflow: hidden;
+      .el-scrollbar__wrap {
+        overflow: scroll;
+        height: calc(100% + 17px);
+        .el-scrollbar__view {
+          .flex-collapse-body {
+            width: 100%
+            &.el-collapse {
+              width: $sideBarWidth - 10px;
+              margin-left: 5px;
+              .el-collapse-item {
+                background-color: $menuColor !important;
+              }
+              .el-collapse-item__header {
+                background-color: $white !important;
+                border-bottom: 1px solid $gray-white !important;
+                font-size: 14px;
+                font-weight: 700;
+                color: $gray-black !important;
+              }
+              .el-collapse-item__header.is-active {
+                border-bottom: transparent !important;
+              }
+              .el-collapse-item__wrap {
+                background-color: $white !important;
+                border-bottom: 1px solid $gray-white !important;
+              }
+              .el-collapse-item__content {
+                color: $grey !important;
+                text-align: left !important;
+                margin: 0 5px 0 5px !important;
+              }
+            }
+          }
+        }
+      }
+      //定义滚动条宽高
+      > .el-scrollbar__bar {
+        z-index: 9999;
+        &.is-horizontal {
+        }
+        &.is-vertical {
+          height: 100%;
+          width: 8px;
+          background-color: transparent;
+        }
+      }
+      //定义滚动条里面的中间条颜色
+      .el-scrollbar__thumb {
+        background-color: $gray-white;
+        border-radius: 4px;
+        transition: background 0.4s;
+      }
+      .el-scrollbar__thumb:hover {
+        background: $grey;
+      }
+    }
+  }
+</style>
---
+++ src/components/Navigation/NavigationMenu.vue
@@ -0,0 +1,215 @@
+<template>
+  <div class="flex-menu-container">
+    <el-scrollbar class="flex-menu-scroll" :native = "false">
+      <el-menu class="flex-menu-body"
+        :default-active="defaultActive"
+        :collapse="false"
+        border-right="0"
+        :background-color="variables.menuColor"
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+        @open="handleOpen"
+        @select="handleSelect"
+        v-for="(route, idx) in menuItems" :key="idx">
+        <el-menu-item :index="route.name" :route="route.path" v-if="route.children == null && route.auth">
+          <div class="box-center">
+            <i :class="route.icon + ' box-right'"></i>
+            <span class="box-left">{{route.title}}</span>
+          </div>
+        </el-menu-item>
+        <el-submenu :index="route.name" v-if="route.children != null && route.auth" >
+          <template slot="title">
+            <div class="box-center">
+              <i :class="route.icon + ' box-right'"></i>
+              <span class="box-left">{{route.title}}</span>
+            </div>
+          </template>
+          <el-menu-item :index="item.name" :route="route.path"
+                        v-for="(item, idx) in route.children" :key="idx">
+            <div class="box-center">
+              <i :class="item.icon + ' box-right'"></i>
+              <span class="box-left">{{item.title}}</span>
+            </div>
+          </el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+
+import Variables from '@/styles/variables.scss'
+
+export default {
+  name: 'NavigationMenu',
+  data () {
+    return {
+      none: 0,
+      menuItems: []
+    }
+  },
+  created () {
+    const routes = this.$router.options.routes[2].children
+    routes.forEach(route => {
+      const path = '/demo/' + route.path
+      let items = []
+      if ('children' in route) {
+        const children = [...route.children]
+        children.forEach(route => {
+          items.push({ name: route.name, title: route.meta.title, auth: route.meta.auth, icon: route.meta.icon, path: path + '/' + route.path, children: null })
+        })
+      } else {
+        items = null
+      }
+
+      this.menuItems.push({ name: route.name, title: route.meta.title, auth: route.meta.auth, icon: route.meta.icon, path: path, children: items })
+    })
+  },
+  computed: {
+    variables () {
+      return Variables
+    },
+    defaultActive () {
+      return this.$store.state.tabPaneTitle
+    },
+    showBrowserPage () {
+      return this.showBrowserWeb || this.showBrowserApp
+    },
+    showBrowserWeb () {
+      return this.$store.state.userAuth.browseWeb !== 0
+    },
+    showBrowserApp () {
+      return this.$store.state.userAuth.browseApp !== 0
+    },
+    showSystemManage () {
+      return this.showSystemQuery || this.showSystemAuth || this.showSystemLog
+    },
+    showSystemAuth () {
+      return this.$store.state.userAuth.systemAuth !== 0
+    },
+    showSystemLog () {
+      return this.$store.state.userAuth.systemLog !== 0
+    },
+    showSystemQuery () {
+      return this.showQueryAuth || this.showQueryPostChange || this.showQueryOrgDelete
+    },
+    showQueryAuth () {
+      return this.$store.state.userAuth.queryAuth !== 0
+    },
+    showQueryPostChange () {
+      return this.$store.state.userAuth.queryPostChange !== 0
+    },
+    showQueryOrgDelete () {
+      return this.$store.state.userAuth.queryOrgDelete !== 0
+    },
+    showPlanCompute () {
+      return this.showPlanParams || this.showPlanStatic || this.showPlanEstimate || this.showPlanActual || this.showPlanTree
+    },
+    showPlanParams () {
+      return this.$store.state.userAuth.planParams !== 0
+    },
+    showPlanStatic () {
+      return this.$store.state.userAuth.planStatic !== 0
+    },
+    showPlanEstimate () {
+      return this.$store.state.userAuth.planEstimate !== 0
+    },
+    showPlanActual () {
+      return this.$store.state.userAuth.planActual !== 0
+    },
+    showPlanTree () {
+      return this.$store.state.userAuth.planTree !== 0
+    },
+    showApiHolder () {
+      return this.$store.state.userAuth.apiHolder !== 0
+    }
+  },
+  watch: {},
+  methods: {
+    handleSelect (index) {
+      this.$store.dispatch('updateTabPaneName', index)
+    },
+    // 鉴权函数
+    handleOpen (key, keyPath) {
+      // console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .flex-menu-container >>> {
+    flex-display();
+    flex-direction(column);
+    flex-justify-content(space-between);
+    height: 60vh;
+    .flex-menu-scroll {
+      flex(1);
+      white-space: nowrap;
+      overflow: hidden;
+      .el-scrollbar__wrap {
+        overflow: scroll;
+        height: calc(100% + 17px);
+        .el-scrollbar__view {
+          .flex-menu-body {
+            width: 100%
+            &.el-menu {
+              border: none;
+              width: $sideBarWidth;
+              max-height: calc(100vh - 100px);
+              .el-menu-item, .el-submenu {
+                //border: 1px solid $menuText;
+                width: $sideBarWidth;
+              }
+
+              .el-menu-item i, .el-submenu i {
+                color: $menuText;
+                //border: 1px solid $menuText;
+                width: 40px;
+              }
+
+              .el-menu-item span, .el-submenu span {
+                color: $menuText;
+                //border: 1px solid $menuText;
+                width: $sideBarWidth - 40px;
+              }
+
+              .el-submenu>.el-submenu__title.el-submenu__title .el-submenu__icon-arrow{
+                color: $menuText;
+                //border: 1px solid $menuText;
+                width: 20px;
+              }
+
+              .el-submenu.is-opened>.el-submenu__title.el-submenu__title .el-submenu__icon-arrow{
+                color: $menuText;
+                //border: 1px solid $menuText;
+                width: 20px;
+              }
+            }
+          }
+        }
+      }
+      //定义滚动条宽高
+      > .el-scrollbar__bar {
+        z-index: 9999;
+        &.is-horizontal {
+        }
+        &.is-vertical {
+          height: 100%;
+          width: 8px;
+          background-color: transparent;
+        }
+      }
+      //定义滚动条里面的中间条颜色
+      .el-scrollbar__thumb {
+        background-color: $gray-white;
+        border-radius: 4px;
+        transition: background 0.4s;
+      }
+      .el-scrollbar__thumb:hover {
+        background: $grey;
+      }
+    }
+  }
+</style>
